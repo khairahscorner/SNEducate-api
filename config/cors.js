7eca338dc5b9deafca415c28004697967e5643fc
Airah Yusuff <airahyusuff@gmail.com>
2023-07-27 20:11:58 +0300

    added prod frontend url

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const cors = require('cors');
 
-const allowedOrigins = [`http://localhost:${process.env.PORT}`, process.env.PROD_API_URL, process.env.BASE_FRONTEND_URL]
+const allowedOrigins = [
+  `http://localhost:${process.env.PORT}`,
+  process.env.PROD_API_URL,
+  process.env.PROD_FRONTEND_URL,
+  process.env.FRONTEND_URL
+]
 
 const corsOptions = {
   origin: allowedOrigins,
